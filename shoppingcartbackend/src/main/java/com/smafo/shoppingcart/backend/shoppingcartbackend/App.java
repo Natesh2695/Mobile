d8bfd5c4d3c33c5effce05a7b456fb71af499cc3
NATESH2695 <natesh2695@gmail.com>
2016-10-18 18:08:26 +0530

    This is my first commit

---
+++ shoppingcartbackend/src/main/java/com/smafo/shoppingcart/backend/shoppingcartbackend/App.java
@@ -0,0 +1,13 @@
+package com.smafo.shoppingcart.backend.shoppingcartbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
